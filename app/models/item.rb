6f97f7cf6246492905559391dc7637d01724bbc7
Normz <johnnormancapule@gmail.com>
2012-07-05 16:23:06 +0800

    59645(Changes in Product Backlogs are logged in History as Anonymous use)

--- app/models/item.rb
+++ app/models/item.rb
@@ -47,10 +47,6 @@ class Item < ActiveRecord::Base
       params[:issue].delete(:status_id)
     end
     
-    if item.points != params[:item][:points].to_i
-      journal.details << JournalDetail.new(:property => 'attr', :prop_key => 'story_points', :old_value => item.points, :value => params[:item][:points])
-    end 
-    
     item.issue.update_attributes! params[:issue]
     item.remove_from_list    
     item.update_attributes! params[:item]
